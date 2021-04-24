03505c6a84900024684f503b8e69f0e7c160061a
Tselate <Tselatedawit@outlook.com>
2021-04-23 23:24:42 -0600

    Last touches

--- src/contact.js
+++ src/contact.js
@@ -2,20 +2,26 @@ export let dominant_three = document.createElement("div");
 
 export function contact () {
     let main = document.getElementById("content");
-    let welcome = document.createElement("div");
-    let order = document.createElement("button");
+    let contact_title = document.createElement("div");
+    let phone = document.createElement("div");
+    let email = document.createElement("button");
+    let last = document.createElement("div");
 
     //Add class list 
     dominant_three.classList.add("dominant_three");
-    welcome.classList.add("welcome");
-    order.classList.add("order");
+    contact_title.classList.add("contact-title");
+    phone.classList.add("phone");
+    email.classList.add("email");
+    last.classList.add("last");
 
     //Add text 
-    welcome.innerHTML = "Yayayay Best Ethiopian Restaurant in town!";
-    order.innerHTML = "Order Now";   
+    contact_title.innerHTML = "Contact us anytime!";
+    phone.innerHTML = "Call us: 000-000-0000";  
+    email.innerHTML = "Email us";
+    last.innerHTML = "Last image";
 
     //Append
-    dominant_three.append(welcome, order);
+    dominant_three.append(contact_title, phone, email, last);
     main.append(dominant_three);
     return;    
 }
--- src/menu.js
+++ src/menu.js
@@ -3,14 +3,17 @@ export function menu () {
     let main = document.getElementById("content");
     let title = document.createElement("div");
     let items = document.createElement("div");
+    let wine = document.createElement("div");
 
     //Add class list 
     dominant_two.classList.add("dominant_two");
     title.classList.add("menu-title");
     items.classList.add("menu-items");
+    wine.classList.add("wine");
 
     //Add text 
     title.innerHTML = "Menu";
+    wine.innerHTML = "Wine";
     
     
     //Make menu items 
@@ -18,7 +21,7 @@ export function menu () {
     let tibs = document.createElement("div");
     let tibs_img = document.createElement("img");
     let tibs_p = document.createElement("p");
-    tibs_p.innerHTML = "Best Tibs.............$10.00";
+    tibs_p.innerHTML = "Best Tibs....................$10.00";
     tibs.appendChild(tibs_img);
     tibs.appendChild(tibs_p);
 
@@ -26,7 +29,7 @@ export function menu () {
     let aynet = document.createElement("div");
     let aynet_img = document.createElement("img");
     let aynet_p = document.createElement("p");
-    aynet_p.innerHTML = "Best Beiyanetu...............$15.00";
+    aynet_p.innerHTML = "Best Beiyanetu..........$15.00";
     aynet.appendChild(aynet_img)
     aynet.appendChild(aynet_p);
 
@@ -35,7 +38,7 @@ export function menu () {
     let tej = document.createElement("div");
     let tej_img = document.createElement("img");
     let tej_p = document.createElement("p");
-    tej_p.innerHTML = "Best Tej...............$3.00";
+    tej_p.innerHTML = "Best Tej......................$3.00";
     tej.appendChild(tej_img)
     tej.appendChild(tej_p);
 
@@ -43,7 +46,7 @@ export function menu () {
     items.append(tibs, tej, aynet);
 
     //Append 
-    dominant_two.append(title, items);
+    dominant_two.append(title, items, wine);
     main.append(dominant_two);
 
     console.log("Works")
